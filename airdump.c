5fd9b6e80c0466a344a87d7b361e6f778c4ab7c0
coconutwaffle <aprilwolf1@gmail.com>
2025-01-12 19:40:29 +0900

    First commit Just show all beacon packet Use sudo ./airump <interface>

---
+++ airdump.c
@@ -0,0 +1,248 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <linux/if_packet.h>
+#include <linux/if_ether.h>
+#include <errno.h>
+#include <ctype.h>
+
+#include "airdump.h"
+
+void print_addr(char *str, addr adr)
+{
+    unsigned char *ptr=adr;
+    if(str) printf("%s", str);
+    for(int i=0;i<6;i++)
+    {
+        printf("%02x:",ptr[i]);
+    }
+    printf("\b \n");
+}
+
+// HEX 덤프 함수
+void hexDump(const void *buffer, size_t size) {
+    const unsigned char *data = (const unsigned char *)buffer;
+    size_t i, j;
+
+    printf("Address         Hexadecimal Values                    ASCII\n");
+    printf("---------------------------------------------------------------\n");
+
+    for (i = 0; i < size; i += 16) {
+        // 주소 출력
+        printf("%08zx  ", i);
+
+        // HEX 출력
+        for (j = 0; j < 16; ++j) {
+            if (i + j < size)
+                printf("%02x ", data[i + j]);
+            else
+                printf("   ");  // 남은 공간 채우기
+        }
+
+        printf(" ");
+
+        // ASCII 출력
+        for (j = 0; j < 16; ++j) {
+            if (i + j < size) {
+                unsigned char c = data[i + j];
+                printf("%c", isprint(c) ? c : '.');  // 출력 가능한 문자 아니면 '.' 출력
+            }
+        }
+
+        printf("\n");
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int sockfd;
+    struct ifreq ifr;
+    struct sockaddr_ll sll;
+    int ret;
+
+    if(argc!=2)
+    {
+        fprintf(stderr, "Use: airodump <interface>\n");
+        return 2;
+    }
+
+    // 1. 소켓 생성: PF_PACKET / SOCK_RAW / ETH_P_ALL(모든 이더타입 캡처)
+    sockfd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+    if (sockfd < 0) {
+        perror("socket");
+        return 2;
+    }
+
+    // 2. 인터페이스 인덱스 가져오기
+    memset(&ifr, 0, sizeof(ifr));
+    strncpy(ifr.ifr_name, argv[1], IFNAMSIZ - 1);
+
+    if (ioctl(sockfd, SIOCGIFINDEX, &ifr) == -1) {
+        perror("ioctl - SIOCGIFINDEX");
+        close(sockfd);
+        return 2;
+    }
+
+    // 3. 소켓 주소 구조체 설정
+    memset(&sll, 0, sizeof(sll));
+    sll.sll_family   = AF_PACKET;
+    sll.sll_ifindex  = ifr.ifr_ifindex;
+    sll.sll_protocol = htons(ETH_P_ALL);
+
+    // 4. 바인드
+    ret = bind(sockfd, (struct sockaddr *)&sll, sizeof(sll));
+    if (ret < 0) {
+        perror("bind");
+        close(sockfd);
+        return EXIT_FAILURE;
+    }
+
+    // 준비 완료 후 패킷 수신 루프로 이동
+    printf("Interface %s is ready in monitor mode. Starting to capture...\n", ifr.ifr_name);
+
+
+    // 5. 패킷 수신 루프
+    while (1) {
+        unsigned char buf[65536];
+        struct beacon_frame *beacon;
+        struct ieee80211_radiotap_header *radiotap = (struct ieee80211_radiotap_header *)buf;
+        ssize_t num_bytes = recvfrom(sockfd, buf, sizeof(buf), 0, NULL, NULL);
+        if (num_bytes < 0) {
+            perror("recvfrom");
+            break;
+        }
+        //hexDump(buf, num_bytes);
+        beacon = (struct beacon_frame *)(buf + radiotap->it_len);
+        if(beacon->magic == 0x0080)
+        {
+            printf("Captured %zd bytes\n", num_bytes);
+            parse_radiotap_body(radiotap->it_present, buf+sizeof(struct ieee80211_radiotap_header), beacon);
+            print_addr("BSS ID: ",beacon->bss_id);
+            parse_becon_body((void *)beacon + sizeof(struct beacon_frame), buf+num_bytes);
+            printf("----------\n");
+        }
+
+    }
+
+    close(sockfd);
+    return 0;
+}
+
+void parse_becon_body(void *start, void *end)
+{
+    uint64_t *timestamp;
+    uint16_t *interver;
+    uint16_t *cap;
+    timestamp = start;
+    interver = start + 8;
+    cap = start + 10;
+    printf("Timestamp: 0x%lx\n", *timestamp);
+    printf("Interval: 0x%x\n", *interver);
+    printf("Capa: 0x%x\n", *cap);
+
+    for(void *pos=start+12;pos<end;)
+    {
+        uint8_t tag_num = *(u_int8_t *)pos;
+        uint8_t len = *(u_int8_t *)(pos+1);
+        pos+=2;
+        switch (tag_num)
+        {
+            case TAG_SSID_NAME:
+                printf("SSID(%d): %.*s\n", (int)len, (int)len, (char *)pos);
+                break;
+            case TAG_RSN:
+            default:
+                break;
+        }
+        pos += len;
+    }
+}
+
+#define NOT_YET() {asm volatile("int $3");}
+void parse_radiotap_body(uint32_t flags, void *start, void *end)
+{
+    void *pos=start;
+    
+    if (flags & RADIOTAP_EXT)
+    {
+        pos +=4;
+    }
+
+    
+    if (flags & RADIOTAP_TSFT)
+    { 
+        //printf("TST: 0x%lx\n", *(uint64_t *)pos);
+        pos += 8;
+    }
+    if (flags & RADIOTAP_FLAGS)
+    { 
+        //printf("flags: 0x%x\n", *(char *)pos);
+        pos += 1;
+    } 
+    if (flags & RADIOTAP_RATE)                 { pos += 1; }  // Rate
+    if (flags & RADIOTAP_CHANNEL)              { pos += 4; }  // Channel
+    if (flags & RADIOTAP_FHSS)                 { pos += 2; }  // FHSS
+    if (flags & RADIOTAP_ANTENNA_SIGNAL)
+    { 
+        printf("dBm: %d\n", *(char *)pos);
+        pos += 1;
+    }  // Antenna signal
+    if (flags & RADIOTAP_ANTENNA_NOISE)        { pos += 1; }  // Antenna noise
+    if (flags & RADIOTAP_LOCK_QUALITY)         { pos += 2; }  // Lock quality
+    if (flags & RADIOTAP_TX_ATTENUATION)       { pos += 2; }  // TX attenuation
+    if (flags & RADIOTAP_DB_TX_ATTENUATION)    { pos += 2; }  // dB TX attenuation
+    if (flags & RADIOTAP_DBM_TX_POWER)         { pos += 1; }  // dBm TX power
+    if (flags & RADIOTAP_ANTENNA)              { pos += 1; }  // Antenna
+    if (flags & RADIOTAP_DB_ANTENNA_SIGNAL)    { pos += 1; }  // dB antenna signal
+    if (flags & RADIOTAP_DB_ANTENNA_NOISE)     { pos += 1; }  // dB antenna noise
+    if (flags & RADIOTAP_RX_FLAGS)             { pos += 2; }  // RX flags
+    if (flags & RADIOTAP_TX_FLAGS)             { pos += 2; }  // TX flags
+    if (flags & RADIOTAP_MCS)                  { pos += 3; }  // MCS
+    if (flags & RADIOTAP_AMPDU_STATUS)         { pos += 8; }  // A-MPDU status
+    if (flags & RADIOTAP_VHT)                  { pos += 12; }  // VHT
+    if (flags & RADIOTAP_TIMESTAMP)            { pos += 12; }  // Timestamp
+    if (flags & RADIOTAP_HE)                   { pos += 12; }  // HE
+    if (flags & RADIOTAP_HE_MU)                { pos += 12; }  // HE-MU
+    if (flags & RADIOTAP_HE_MU_OTHER_USER)     { pos += 6; }  // HE-MU-other-user
+    if (flags & RADIOTAP_ZERO_LENGTH_PSDU)     { pos += 1; }  // 0-length-PSDU
+    if (flags & RADIOTAP_L_SIG)                { pos += 4; }  // L-SIG
+    if (flags & RADIOTAP_TLV_FIELDS)
+    { 
+
+	int length = 4 + *(uint16_t *)(pos + 2);
+	int padding = (4 - (length & 3)) & 3;
+        pos += length + padding;
+
+    }  // TLV fields
+    if (flags & RADIOTAP_RADIOTAP_NAMESPACE)   { NOT_YET();}  // Radiotap Namespace
+    if (flags & RADIOTAP_VENDOR_NAMESPACE)
+    { 
+        pos += 6 + *(uint16_t *)(pos+4);
+    }  // Vendor Namespace
+    if (flags & RADIOTAP_EXT)
+    {
+        flags = *(uint32_t *)start;
+        if (flags & RADIOTAP_S1G)                  { pos += 6; }  // S1G
+        if (flags & RADIOTAP_U_SIG)                { pos += 8; }  // U-SIG
+        if (flags & RADIOTAP_EHT)
+        {
+            //TODO Need to replace 0 to acutal user_num	
+            NOT_YET();
+            pos += 40 + 4*(0);
+        }  // EHT
+     }
+
+
+    if(pos != end)
+    {
+        fprintf(stderr, "radiotap: %p, %p, %p\n", start, pos, end);
+	    fprintf(stderr, "radiotap header: parsing fail\n");
+        NOT_YET();
+    }
+}
---
+++ airdump.h
@@ -0,0 +1,64 @@
+#ifndef AIRDUMP_25_1_10
+#define AIRDUMP_25_1_10
+
+struct ieee80211_radiotap_header {
+        u_int8_t        it_version;
+        u_int8_t        it_pad;
+        u_int16_t       it_len;
+        u_int32_t       it_present;
+} __attribute__((__packed__));
+
+typedef char addr[6];
+struct beacon_frame {
+    u_int16_t magic;
+    u_int16_t dur;
+    addr dst_addr;
+    addr src_addr;
+    addr bss_id;
+    u_int16_t extra;
+} __attribute__((__packed__));
+
+
+#define TAG_SSID_NAME 0
+#define TAG_RSN 48
+
+
+
+//https://www.radiotap.org/fields/defined
+#define RADIOTAP_TSFT                 (1 << 0)   // TSFT
+#define RADIOTAP_FLAGS                (1 << 1)   // Flags
+#define RADIOTAP_RATE                 (1 << 2)   // Rate
+#define RADIOTAP_CHANNEL              (1 << 3)   // Channel
+#define RADIOTAP_FHSS                 (1 << 4)   // FHSS
+#define RADIOTAP_ANTENNA_SIGNAL       (1 << 5)   // Antenna signal
+#define RADIOTAP_ANTENNA_NOISE        (1 << 6)   // Antenna noise
+#define RADIOTAP_LOCK_QUALITY         (1 << 7)   // Lock quality
+#define RADIOTAP_TX_ATTENUATION       (1 << 8)   // TX attenuation
+#define RADIOTAP_DB_TX_ATTENUATION    (1 << 9)   // dB TX attenuation
+#define RADIOTAP_DBM_TX_POWER         (1 << 10)  // dBm TX power
+#define RADIOTAP_ANTENNA              (1 << 11)  // Antenna
+#define RADIOTAP_DB_ANTENNA_SIGNAL    (1 << 12)  // dB antenna signal
+#define RADIOTAP_DB_ANTENNA_NOISE     (1 << 13)  // dB antenna noise
+#define RADIOTAP_RX_FLAGS             (1 << 14)  // RX flags
+#define RADIOTAP_TX_FLAGS             (1 << 15)  // TX flags
+#define RADIOTAP_MCS                  (1 << 19)  // MCS
+#define RADIOTAP_AMPDU_STATUS         (1 << 20)  // A-MPDU status
+#define RADIOTAP_VHT                  (1 << 21)  // VHT
+#define RADIOTAP_TIMESTAMP            (1 << 22)  // Timestamp
+#define RADIOTAP_HE                   (1 << 23)  // HE
+#define RADIOTAP_HE_MU                (1 << 24)  // HE-MU
+#define RADIOTAP_HE_MU_OTHER_USER     (1 << 25)  // HE-MU-other-user
+#define RADIOTAP_ZERO_LENGTH_PSDU     (1 << 26)  // 0-length-PSDU
+#define RADIOTAP_L_SIG                (1 << 27)  // L-SIG
+#define RADIOTAP_TLV_FIELDS           (1 << 28)  // TLV fields in radiotap
+#define RADIOTAP_RADIOTAP_NAMESPACE   (1 << 29)  // Radiotap Namespace
+#define RADIOTAP_VENDOR_NAMESPACE     (1 << 30)  // Vendor Namespace
+#define RADIOTAP_EXT                  (1 << 31)
+#define RADIOTAP_S1G                  (1 << 0)  // S1G
+#define RADIOTAP_U_SIG                (1 << 1)  // U-SIG
+#define RADIOTAP_EHT                  (1 << 2)  // EHT
+
+void parse_radiotap_body(u_int32_t flags, void *start, void *end);
+void parse_becon_body(void *start, void *end);
+
+#endif
